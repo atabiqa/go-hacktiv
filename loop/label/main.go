6c49404d20347f06c04d8b2704d701f9192c1783
atabiqa <atabiqasadlearn@gmail.com>
2024-02-20 08:14:17 +0800

    label

---
+++ loop/label/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+outerLoop:
+
+	for i := 0; i < 3; i++ {
+		fmt.Println("perulangan ke-", i+1)
+		for j := 0; j < 3; j++ {
+			if i == 2 {
+				break outerLoop
+			}
+			fmt.Println(j, " ")
+		}
+		fmt.Println("\n")
+	}
+}
