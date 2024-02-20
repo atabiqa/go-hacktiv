6ddba6a3ba13de8b42d3bc8027a7223d922a8939
atabiqa <atabiqasadlearn@gmail.com>
2024-02-21 07:56:43 +0800

    byte by bytee

---
+++ strings-in-depth/byte-by-byte/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	// city := "jakarta"
+
+	// for i := 0; i < len(city); i++ {
+	// 	fmt.Printf("index: %d, byte: %d\n", i, city[i])
+	// }
+
+	var city []byte = []byte{74, 97, 107, 97, 114, 116, 97}
+	fmt.Println(string(city))
+}
