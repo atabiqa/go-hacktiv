d7d5eca203ee923941f8ef2ac0d09677ff2d7506
atabiqa <atabiqasadlearn@gmail.com>
2024-02-22 05:23:25 +0800

    belajar-go

--- alias/alias/main.go
+++ alias/alias/main.go
@@ -1,10 +1,10 @@
 package main
 
-func main() {
-	//deklarasi variable dengan tipe data uint8
-	var a uint8 = 10
-	var b byte // byte adalah alias dari tipe data uint8
+//func main() {
+//deklarasi variable dengan tipe data uint8
+//var a uint8 = 10
+//var b byte // byte adalah alias dari tipe data uint8
 
-	b = a // no error, karena byte memiliki tipe data yang sama dengan uint8
-	_ = b
-}
+//b = a // no error, karena byte memiliki tipe data yang sama dengan uint8
+//_ = b
+//}
---
+++ belajar-go/helpers/function.go
@@ -0,0 +1,11 @@
+package helpers
+
+import "fmt"
+
+func Greet() {
+	fmt.Println("hallo dari function Greet")
+}
+
+func greet() {
+	fmt.Println("halo ini dari function greet")
+}
