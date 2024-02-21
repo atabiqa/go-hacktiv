a74f7a618b90bda32c8fb81226a6f80359d5abd5
atabiqa <atabiqasadlearn@gmail.com>
2024-02-22 06:16:15 +0800

    refactori code

--- interface/interface/main.go
+++ interface/interface/main.go
@@ -31,16 +31,25 @@ func (r rectangle) perimeter() float64 {
 	return 2 * (r.height + r.width)
 }
 
+func print(t string, s shape) {
+	fmt.Printf("%s area %v\n", t, s.area())
+	fmt.Printf("%s perimeter %v\n", t, s.perimeter())
+}
+
 func main() {
-	var c1 = circle{radius: 5}
-	var r1 = rectangle{width: 10, height: 5}
+	// var c1 = circle{radius: 5}
+	// var r1 = rectangle{width: 10, height: 5}
 
-	//fmt.Printf("Type of c1 : %T", c1)
-	//fmt.Printf("Type of r1 : %T", r1)
+	// //fmt.Printf("Type of c1 : %T", c1)
+	// //fmt.Printf("Type of r1 : %T", r1)
 
-	fmt.Println("circle area:", c1.area())
-	fmt.Println("circle perimeter:", c1.perimeter())
+	// fmt.Println("circle area:", c1.area())
+	// fmt.Println("circle perimeter:", c1.perimeter())
 
-	fmt.Println("rectangle area:", r1.area())
-	fmt.Println("rectangle perimeter:", r1.perimeter())
+	// fmt.Println("rectangle area:", r1.area())
+	var c1 shape = circle{radius: 5}
+	var r1 shape = rectangle{width: 3, height: 2}
+
+	print("circle", c1)
+	print("rectangle", r1)
 }
