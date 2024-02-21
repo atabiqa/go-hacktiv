d5d28f199ae9ec0a4dd1bc6253caee71090a5038
atabiqa <atabiqasadlearn@gmail.com>
2024-02-21 13:18:16 +0800

    pointer as parameter

---
+++ pointer/pointer-as-parameter/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a int = 10
+
+	fmt.Println("before:", a)
+	changeValue(&a)
+	fmt.Println("after", a)
+}
+
+func changeValue(number *int) {
+	*number = 20
+}
