510b3578202810f904ad5beb48ac2dc37b0b9602
atabiqa <atabiqasadlearn@gmail.com>
2024-02-21 09:59:27 +0800

    predefine-return value

---
+++ fungsi/predefine-return-value/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	var diameter float64 = 15
+
+	var area, circumference = calculated(diameter)
+
+	fmt.Println("area :", area)
+	fmt.Println("circumference:", circumference)
+}
+
+func calculated(d float64) (area float64, circumference float64) {
+	// menghitung luas
+	area = math.Pi * math.Pow(d/2, 2)
+
+	//menghitung keliling
+	circumference = math.Pi * d
+
+	return
+}
