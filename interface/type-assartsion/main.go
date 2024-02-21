c8806d5d7163894618b05f63a5bdf6001b3a5688
atabiqa <atabiqasadlearn@gmail.com>
2024-02-22 07:01:02 +0800

    assertsion

---
+++ interface/type-assartsion/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type shape interface {
+	area() float64
+	perimeter() float64
+}
+
+type circle struct {
+	radius float64
+}
+
+type rectangle struct {
+	width, height float64
+}
+
+func (c circle) area() float64 {
+	return math.Pi * math.Pow(c.radius, 2)
+}
+func (r rectangle) area() float64 {
+	return r.height * r.width
+}
+func (c circle) perimeter() float64 {
+	return 2 * math.Pi * c.radius
+}
+func (r rectangle) perimeter() float64 {
+	return 2 * (r.height + r.width)
+}
+
+func (c circle) volume() float64 {
+	return 4 / 3 * math.Pi * math.Pow(c.radius, 5)
+}
+
+func print(t string, s shape) {
+	fmt.Printf("%s area %v\n", t, s.area())
+	fmt.Printf("%s perimeter %v\n", t, s.perimeter())
+}
+
+func main() {
+	var c1 shape = circle{radius: 5}
+
+	c1.(circle).volume()
+}
