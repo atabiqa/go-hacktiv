69ea7d9bc0263a8bf29dc0804a607f77caebdde1
atabiqa <atabiqasadlearn@gmail.com>
2024-02-22 09:05:18 +0800

    waitgroup

---
+++ waitgroup/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func main() {
+	fruits := []string{"apple", "banana", "mango", "durian"}
+
+	var wg sync.WaitGroup
+
+	for index, fruit := range fruits {
+		wg.Add(1)
+		go printFruit(index, fruit, &wg)
+	}
+	wg.Wait()
+}
+
+func printFruit(index int, fruit string, wg *sync.WaitGroup) {
+	fmt.Printf("index ==>%d, fruit ==> %s\n", index, fruit)
+	wg.Done()
+}
