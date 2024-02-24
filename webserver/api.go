6bcb40fae00692a766f94783b6edf7c4980e4694
atabiqa <atabiqasadlearn@gmail.com>
2024-02-24 14:04:11 +0800

    webserver

---
+++ webserver/api.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"html/template"
+	"net/http"
+	"strconv"
+)
+
+type Employee struct {
+	ID       int
+	Name     string
+	Age      int
+	Division string
+}
+
+var employees = []Employee{
+	{ID: 1, Name: "Airel", Age: 23, Division: "IT"},
+	{ID: 2, Name: "Nanda", Age: 23, Division: "Finance"},
+	{ID: 3, Name: "Mario", Age: 20, Division: "IT"},
+}
+
+var PORT = ":8080"
+
+func main() {
+	http.HandleFunc("/employees", getEmployees)
+	http.HandleFunc("/employee", createEmployee)
+
+	fmt.Println("Application is listening on port", PORT)
+	http.ListenAndServe(PORT, nil)
+}
+
+func getEmployees(w http.ResponseWriter, r *http.Request) {
+
+	if r.Method == "GET" {
+		tpl, err := template.ParseFiles("template.html")
+
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		tpl.Execute(w, employees)
+		return
+	}
+
+	http.Error(w, "Invalid methode", http.StatusBadRequest)
+}
+
+func createEmployee(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Conten-Type", "application/json")
+
+	if r.Method == "POST" {
+		name := r.FormValue("name")
+		age := r.FormValue("age")
+		division := r.FormValue("division")
+
+		convertAge, err := strconv.Atoi(age)
+
+		if err != nil {
+			http.Error(w, "invalid age", http.StatusBadRequest)
+			return
+		}
+
+		newEmployee := Employee{
+			ID:       len(employees) + 1,
+			Name:     name,
+			Age:      convertAge,
+			Division: division,
+		}
+
+		employees = append(employees, newEmployee)
+
+		json.NewEncoder(w).Encode(newEmployee)
+		return
+	}
+
+	http.Error(w, "invalid method", http.StatusBadRequest)
+}
